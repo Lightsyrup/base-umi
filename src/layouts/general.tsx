534c56401ba4fea303d38c4fb1b854f0fab7bf22
Charlene <postcard223@163.com>
2021-11-30 10:18:11 +0800

    feat:add <Footer> component

--- src/layouts/general.tsx
+++ src/layouts/general.tsx
@@ -1,6 +1,17 @@
-import { useState } from 'react';
+import React from 'react';
+import TopNav from '@/components/topNav';
 import styles from './general.less';
+import { Layout } from 'antd';
 
-const GeneralLayout: React.FC = (props): any => {};
+const GeneralLayout: React.FC = (props): any => {
+  return (
+    <Layout>
+      <TopNav />
+      <Layout>
+      
+      </Layout>
+    </Layout>
+  );
+};
 
 export default GeneralLayout;
